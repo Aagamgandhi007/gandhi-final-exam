2a6d595b6d4c8f305095a75310cc77879a29d640
Aagamgandhi007 <aagam2900@gmail.com>
2025-04-10 12:15:05 -0400

    Initial Flask app for final exam

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask
+import socket
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return "Welcome to gandhi Final Test API Server"
+def home():
+    return "Welcome to gandhi Final Exam Test API Server"
 
 @app.route('/host')
 def host():
-    return "Host route works!"
+    hostname = socket.gethostname()
+    return f"Host route works! Hostname: {hostname}"
 
 @app.route('/ip')
 def ip():
-    return "IP route works!"
+    ip_address = socket.gethostbyname(socket.gethostname())
+    return f"IP route works! IP: {ip_address}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
